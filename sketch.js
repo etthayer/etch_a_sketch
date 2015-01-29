307a1900348df68bac86b32bff357ce7c23ba91b
etthayer <etthayer@gmail.com>
2015-01-29 20:36:39 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-
+  $('#press').on('click', function(){
+    var x = 0;
+    while (x < 500) {
+      $('.container').append('<div class = grid></div>');
+      x++; 
+    };
+    
+    $('.grid').on('mouseenter', function(){
+      $(this).css("opacity": "1");
+    });
+  });
 
 });
